7054a36d1544d79936ef3bb21f94ca207fb3545f
Mihajlo <djmixa26.hockey@gmail.com>
2022-11-16 22:50:57 +0100

    Update GoogleSheets.rb
    Tables adding and subtracting (operators + and - on Table class) have been implemented.

--- GoogleSheets.rb
+++ GoogleSheets.rb
@@ -5,23 +5,25 @@ require "google_drive"
 # first time and save it to config.json file for later usages.
 # See this document to learn how to create config.json:
 # https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
-session = GoogleDrive::Session.from_config("config.json")
+$session = GoogleDrive::Session.from_config("config.json")
 
 # First worksheet of
 # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
 # Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
-$ws = session.spreadsheet_by_key("1JSNrQrqQZkiVOd5QT10Lm5OKUh5jD5U6BnmCfjramjI").worksheets[0]
+$ws = $session.spreadsheet_by_key("1JSNrQrqQZkiVOd5QT10Lm5OKUh5jD5U6BnmCfjramjI").worksheets[0]
   
 $currentSheet = 0
 
 #Opens a sheet specified by the argument
 def open_sheet(sheetNumber)
-  $ws = session.spreadsheet_by_key("1JSNrQrqQZkiVOd5QT10Lm5OKUh5jD5U6BnmCfjramjI").worksheets[sheetNumber]
+  $ws = $session.spreadsheet_by_key("1JSNrQrqQZkiVOd5QT10Lm5OKUh5jD5U6BnmCfjramjI").worksheets[sheetNumber]
   $currentSheet = sheetNumber
 end
 
 class Table
   include Enumerable
+
+  attr_accessor :table
 
   def initialize(worksheet)
     @worksheet = worksheet
@@ -46,7 +48,6 @@ class Table
 
   def row_empty?(row)
     (1..$ws.num_cols).each do |col|
-      #print("$ws[row, col].empty? is #{$ws[row, col]}\n")
       return false unless $ws[row, col].empty? || $ws[row, col].nil?
     end
     true
@@ -60,6 +61,14 @@ class Table
     false
   end
 
+  def  get_row(table_col_pos, row)
+    dummy_row = []
+    (table_col_pos..$ws.num_cols).each do |col|
+      dummy_row[col-table_col_pos] = $ws[row, col]
+    end
+    dummy_row
+  end
+
   def load_table
     table_row, table_col = find_table
     @table = []
@@ -70,16 +79,79 @@ class Table
         next
       end
 
-      dummy_row = []
-      (table_col..$ws.num_cols).each do |col|
-        dummy_row[col-table_col] = $ws[row, col]
-      end
-      @table[row-table_row] = dummy_row
+      @table[row-table_row] = get_row(table_col, row)
     end
   end
 
   def row(index)
     @table[index]
+  end
+
+  def row_exists?(table, row_to_compare)
+    (0..table.size-1).each do |row|
+      same = true
+      (0..table[0].size-1).each do |col|
+        same = false unless table[row][col] == row_to_compare[col]
+      end
+      return true if same
+    end
+    false
+  end
+
+  def headers_equal?(table1, table2)
+    return false unless table1.size == table2.size
+    (0..table1.size).each do |col|
+      return false unless table1[0][col] == table2[0][col]
+    end
+    true
+  end
+
+  def +(table)
+    return Table.new(-1) unless headers_equal?(@table, table.table)
+
+    table_result = []
+    result_row = 0
+    
+    (0..@table.size-2).each do |row|
+      dummy_row = []
+      (0..@table[0].size-1).each do |col|
+        dummy_row[col] = @table[row+1][col]
+      end
+      table_result[result_row] = dummy_row
+      result_row += 1
+    end
+
+    (0..table.table.size-2).each do |row|
+      dummy_row = []
+      (0..table.table[0].size-1).each do |col|
+        dummy_row[col] = table.table[row+1][col]
+      end
+      next if row_exists?(table_result, dummy_row)
+      table_result[result_row] = dummy_row
+      result_row += 1
+    end    
+
+    table_return = Table.new(-1)
+    table_return.table = table_result
+    table_return
+  end
+
+  def -(table)
+    return Table.new(-1) unless headers_equal?(@table, table.table)
+
+    table_result = []
+    result_row = 0
+    
+    (0..@table.size-1).each do |row|
+      unless row_exists?(table.table, @table[row])
+        table_result[result_row] = @table[row]
+        result_row += 1
+      end
+    end
+
+    table_return = Table.new(-1)
+    table_return.table = table_result
+    table_return
   end
 
   def print_contents
@@ -91,4 +163,9 @@ end
 
 table = Table.new($currentSheet)
 table.load_table
-print table.row(1)
+open_sheet(1)
+table1 = Table.new($currentSheet)
+table1.load_table
+table2 = table-table1
+table2.print_contents
+
